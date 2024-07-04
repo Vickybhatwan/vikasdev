b2a5d47ee921024a0bd41285544a513ee9d2004c
Vickybhatwan <vikasbhatwan01@gmail.com>
2024-07-04 12:58:53 +0530

    heder change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,20 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <div className="logo">Vikas's Portfolio</div>
-      <nav>
-        <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#blog">Blog</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+      <div className="container">
+        <div className="logo">
+          <img src="/path/to/your/logo.png" alt="Logo" />
+          <span>Vikas</span> {/* Replace with your name or brand */}
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
